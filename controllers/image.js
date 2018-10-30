ece13b001ebba199a38dc163b088cb416fc1119d
Thomas Hanna <thomasjhanna@gmail.com>
2018-10-30 14:21:54 +0000

    Added NODE_ENV variable for production

--- controllers/image.js
+++ controllers/image.js
@@ -1,14 +1,23 @@
 const Clarifai = require("clarifai");
 
-const app = new Clarifai.App({
-  apiKey: "7c773f24579740208678ac924c3abd74"
-});
+const app =
+  process.env.NODE_ENV === "production"
+    ? new Clarifai.App({
+        apiKey: process.env.CLARIFAI_API_KEY
+      })
+    : new Clarifai.App({
+        apiKey: "replace with an API key"
+      });
 
 const handleApiCall = () => (req, res) => {
-  app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(res => res.json())
-    .catch(err => res.status(400).json(err.detail));
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input).then(
+    function(response) {
+      res.json(response);
+    },
+    function(err) {
+      res.status(400).json("Ooops something went wrong with the Clarifai API");
+    }
+  );
 };
 
 const handleImage = db => (req, res) => {
@@ -28,7 +37,36 @@ const handleImage = db => (req, res) => {
     );
 };
 
+// const test = () => (req, res) => {
+//   app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input).then(
+//     function(response) {
+//       res.json(response);
+//     },
+//     function(err) {
+//       res.status(401).json(err);
+//     }
+//   );
+// };
+
+// const simpletest = () => (req, res) => {
+//   app.models
+//     .predict(
+//       Clarifai.FACE_DETECT_MODEL,
+//       "https://samples.clarifai.com/face-det.jpg"
+//     )
+//     .then(
+//       function(response) {
+//         res.json(response);
+//       },
+//       function(err) {
+//         res.status(400).json(err);
+//       }
+//     );
+// };
+
 module.exports = {
   handleImage,
   handleApiCall
+  // test,
+  // simpletest
 };
--- server.js
+++ server.js
@@ -11,17 +11,32 @@ const image = require("./controllers/image");
 
 const PORT = process.env.PORT || 4000;
 
-const db = knex({
-  client: "pg",
-  connection: {
-    connectionString: process.env.DATABASE_URL,
-    ssl: true
-  }
-});
+const db =
+  process.env.NODE_ENV === "production"
+    ? knex({
+        client: "pg",
+        connection: {
+          connectionString: process.env.DATABASE_URL,
+          ssl: true
+        }
+      })
+    : knex({
+        client: "pg",
+        connection: {
+          host: "127.0.0.1",
+          user: "postgres",
+          password: "test",
+          database: "smart-brain"
+        }
+      });
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("It is working!");
+});
 
 // app.get("/", (req, res) => {
 //   db.select("*")
@@ -36,41 +51,13 @@ app.use(cors());
 //     );
 // });
 
-// app.get("/test", (req, res) => {
-//   db.select("id", "name", "email", "entries", "joined")
-//     .from("users")
-//     .then(
-//       users => {
-//         res.json(users);
-//       },
-//       err => {
-//         res.json("Couldn't retrieve users");
-//       }
-//     );
-// });
-
-// app.get("/test2", (req, res) => {
-//   db.select("id", "hash", "email")
-//     .from("login")
-//     .then(
-//       login => {
-//         res.json(login);
-//       },
-//       err => {
-//         res.json("Couldn't retrieve login");
-//       }
-//     );
-// });
-
-app.get("/", (req, res) => {
-  res.send("It is working!");
-});
-
 app.post("/signin", signin.handleSignin(db, bcrypt));
 app.post("/register", register.handleRegister(db, bcrypt));
 app.get("/profile/:userId", profile.handleProfileGet(db));
-app.put("/image", image.handleImage(db));
 app.put("/imageurl", image.handleApiCall());
+app.put("/image", image.handleImage(db));
+// app.post("/test", image.test());
+// app.post("/simpletest", image.test());
 
 app.listen(PORT, () => {
   console.log(`Server is up on port ${PORT}`);
